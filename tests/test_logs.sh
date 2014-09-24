c0592d5671e47145a97ee34770909b44bc425cb5
Adham Helal <ahelal@users.noreply.github.com>
2014-09-24 12:33:05 +0200

    travis test 7

--- tests/test_logs.sh
+++ tests/test_logs.sh
@@ -10,7 +10,7 @@ cd tests
 echo "ls -l"
 
 
-sleep 30
+sleep 4
 cat nginx_logs.txt | nc -v localhost 9877
 cat default_logs_ok.txt | nc -v localhost 9123
 cat default_logs_fail.txt  | nc -v localhost 9124
@@ -18,26 +18,19 @@ cat default_logs_fail.txt  | nc -v localhost 9124
 echo "/tmp ls"
 ls -l /tmp
 
-sleep 20
-echo "java ps"
-ps aux | grep java
-echo "log of logstash"
-cat /var/log/logstash/logstash.log
 other=$(cat /tmp/logstash_all_others.txt | wc -l)
 default_tcp=$(cat /tmp/logstash_default_tcp_9124_match_ok.txt | wc -l)
 nginx_fail=$(cat /tmp/logstash_nginx_9877_match_fail.txt | wc -l)
 nginx_ok=$(cat /tmp/logstash_nginx_9877_match_ok.txt | wc -l )
 
-test_pass(){
-	if [ $1 -nq $2 ]; then
+test_pass () {
+	if [ $1 -ne $2 ]; then
 		echo " [Failed]"
-		exit 1
-	else
+        exit 1
+    else
 		echo " [Ok]"
+    fi
 }
-
-echo -n "All other logs : "
-test_pass $other 1
 
 echo -n "Default tcp logs : "
 test_pass $default_tcp 1

92b376fcffc6a10dc6e62dbd6b5cb2c70d912c9c
Adham Helal <helal@HelalMac-2.local>
2014-09-24 19:31:20 +0200

    travis test 12

--- tests/test_logs.sh
+++ tests/test_logs.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 set -e
 
-echo "pwd"
-pwd
-echo "ls -l"
-ls -l 
-
-cd tests
-echo "ls -l"
-
-
-sleep 4
+echo "sleep 30 to make sure logstash is up"
+sleep 30
+cd  tests
 cat nginx_logs.txt | nc -v localhost 9877
 cat default_logs_ok.txt | nc -v localhost 9123
 cat default_logs_fail.txt  | nc -v localhost 9124
 
-echo "/tmp ls"
-ls -l /tmp
+echo "sleep 30 to make sure logstash parsed data "
+sleep 30
 
 other=$(cat /tmp/logstash_all_others.txt | wc -l)
 default_tcp=$(cat /tmp/logstash_default_tcp_9124_match_ok.txt | wc -l)
 nginx_fail=$(cat /tmp/logstash_nginx_9877_match_fail.txt | wc -l)
 nginx_ok=$(cat /tmp/logstash_nginx_9877_match_ok.txt | wc -l )
 
-test_pass(){
-	if [ $1 -nq $2 ]; then
+test_pass () {
+	if [ $1 -ne $2 ]; then
 		echo " [Failed]"
-		exit 1
-	else
+        exit 1
+    else
 		echo " [Ok]"
+    fi
 }
 
-echo -n "All other logs : "
-test_pass $other 1
-
-echo -n "Default tcp logs : "
+echo -n "checking default tcp logs : "
 test_pass $default_tcp 1
 
-echo -n "Nginx Fail logs : "
+echo -n "checking nginx Fail logs : "
 test_pass $nginx_fail 1
 
-echo -n "Nginx Ok logs : "
+cat /tmp/logstash_nginx_9877_match_ok.txt
+echo -n "checking  nginx Ok logs : "
 test_pass $nginx_ok 2

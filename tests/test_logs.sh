f00f7ccc09b305d2f0b68749fd6da3a29914b5f0
Adham Helal <helal@HelalMac-2.local>
2014-09-24 11:17:16 +0200

    test 2 travis build

--- tests/test_logs.sh
+++ tests/test_logs.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
+echo "pwd"
+pwd
+echo "ls -l"
+ls -l 
+
 cd tests
+echo "ls -l"
+
 
 sleep 4
 cat nginx_logs.txt | nc -v localhost 9877
 cat default_logs_ok.txt | nc -v localhost 9123
 cat default_logs_fail.txt  | nc -v localhost 9124
 
+echo "/tmp ls"
+ls -l /tmp
 
 other=$(cat /tmp/logstash_all_others.txt | wc -l)
 default_tcp=$(cat /tmp/logstash_default_tcp_9124_match_ok.txt | wc -l)
